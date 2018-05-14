c7fd09686f093a85730c904b77c2d16d6e084cb0
Mansi <mansigpatel1@gmail.com>
2018-05-14 16:34:46 -0400

    changed the button text when we click

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const button = document.querySelector('button.greeting');
 
-function changeText() {
-    const paragraph = document.querySelector('p.greeting');
-    paragraph.textContent = 'You done clicked';
+function changeText(ev) {
+    //const paragraph = document.querySelector('p.greeting');
+    //paragraph.textContent = 'You done clicked';
+    ev.target.textContent = 'Clickity click';
 }
 
 button.addEventListener('click', changeText);
