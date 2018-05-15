09b1a0a4eef96e7e371d06a521592ca7bc8b8382
Mansi <mansigpatel1@gmail.com>
2018-05-14 21:38:30 -0400

    changed the button so h1 change

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const button = document.querySelector('button.greeting');
-
+const heading = document.querySelector('h1');
 function changeText(ev) {
     //const paragraph = document.querySelector('p.greeting');
     //paragraph.textContent = 'You done clicked';
     ev.target.textContent = 'Clickity click';
+    heading.textContent = 'Changed User Directory';
 }
 
 button.addEventListener('click', changeText);
