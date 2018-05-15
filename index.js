f8b34c582f9af10b8b3b2430d65d5647541dcd23
Mansi <mansigpatel1@gmail.com>
2018-05-15 14:42:50 -0400

    Add multiple username list

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-const button = document.querySelector('button.greeting');
-const heading = document.querySelector('#h2');
+//const button = document.querySelector('button.greeting');
+//const heading = document.querySelector('#h2');
 
-function changeText(ev) {
+
+//function changeText(ev) {
     //const paragraph = document.querySelector('p.greeting');
     //paragraph.textContent = 'You done clicked';
-    ev.target.textContent = 'Clickity click';
-   heading.textContent = 'Changed User Directory';
+    //ev.target.textContent = 'Clickity click';
+   //heading.textContent = 'Changed User Directory';
+//}
 
+//const txtName = document.getElementById("txtName");
+//const txtOutput = Document.getElementById("txtOutput");
+
+//function changeName() {
+  //  heading.textContent = txtName.value;
+//}
+//button.addEventListener('click', changeText);
+
+
+const form = document.querySelector('#userForm')
+
+const handleSubmit = function(ev) {
+  ev.preventDefault()
+  const heading = document.querySelector("#users")
+  const f = ev.target
+  const userName = f.userName.value
+  users.textContent += ' ' + userName
 }
 
-const txtName = document.getElementById("txtName");
-const txtOutput = Document.getElementById("txtOutput");
-
-function changeName() {
-    heading.textContent = txtName.value;
-}
-button.addEventListener('click', changeText);
+form.addEventListener('submit', handleSubmit)
 
