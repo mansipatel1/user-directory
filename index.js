ce7fb26c56a7a848aba15f766aa6163654dc824e
Mansi <mansigpatel1@gmail.com>
2018-05-15 16:05:25 -0400

    formatting

--- index.js
+++ index.js
@@ -23,24 +23,25 @@ const form = document.querySelector('#userForm')
 const handleSubmit = function(ev) {
   ev.preventDefault();
   const users = document.querySelector('#users');
+  users.appendChild(list);
   const f = ev.target;
   const userName = f.userName.value;
   const age = f.age.value;
   const favoriteColor = f.favoriteColor.value;
   //list.style.backgroundColor = favoriteColor;
 
-  const list = document.createElement('ul')
   
-  const nameItem = document.createElement('li');
+const nameItem = document.createElement('li');
     nameItem.textContent = `Name: ${userName}`;
-    list.appendChild(nameItem);
 
-  const ageItem = document.createElement('li');
+
+const ageItem = document.createElement('li');
     ageItem.textContent = `Age: ${age}`;
-    list.appendChild(ageItem);
 
-  
-  users.appendChild(list);
+
+const list = document.createElement('ul')
+  list.appendChild(nameItem);
+  list.appendChild(ageItem);
 
   f.reset()
   f.userName.focus()
